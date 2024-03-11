9814eafb87d8d53eb09de159bba2971b1c9d4177
Khudoykulov <diyorbekxudoyqulov627@gmail.com>
2024-03-11 14:16:20 +0500

    yasg

--- account/views.py
+++ account/views.py
@@ -1,3 +1,6 @@
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
@@ -18,6 +21,27 @@ def logout_view(request):
     return Response(response, status=status.HTTP_200_OK)
 
 
+@swagger_auto_schema(
+    method="POST",
+    reuqest_body=openapi.Schema(
+        type="object",
+        properties={
+            "username": openapi.Schema(type=openapi.TYPE_STRING),
+            "password1": openapi.Schema(type=openapi.TYPE_STRING),
+            "password2": openapi.Schema(type=openapi.TYPE_STRING),
+        },
+        required=["username", "password1", "password2"],
+        example={
+            "username": "",
+            "password1": "",
+            "password2": ""
+        }
+    ),
+    responses={
+        200: openapi.Response(description="Success response"),
+        400: openapi.Response(description="Bad request"),
+    }
+)
 @api_view(["POST"])
 def register_view(request):
     data = request.data
